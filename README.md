ca790506795542d0653e159f23275c54449198b9
Mateus Goncalves de Oliveira <mateus.disp@gmail.com>
2017-10-03 03:58:33 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # ces26
+
+Tarefa 1:
+
+Criar um programa Javascript que explore a interface
+DOM.
+Cria e remove elementos.
+Modifica elementos.
+Acrescenta manipuladores de eventos.
+
+Tarefa 2:
+Crie exemplos de testes com:
+Elementos de vídeo.
+Geo-localização e mapas.
+Web worker.
+Desenvolva testes mais complexos que os ilustrados nos
+tutoriais.
+No projeto procure usar as novas funcionalidades do
+HTML 5.
+
+Tarefa 4:
+Criar um servidor que processa formulários GET e POST.
+Inclua processamento de cookies.
+Inclua armazenamento de dados no servidor. Utilize o
+módulo de arquivos do Node.
+
+Tarefa 5:
+Criar um exemplo com Jquery.
+Explorar operações para percorrer a árvore DOM.
+Explorar efeitos (animação, fading,...).
+Usar AJAX: Carregar um arquivo JSON e atualizar parte do
+conteúdo da página com o conteúdo recebido.
---
+++ index.js
@@ -0,0 +1,41 @@
+var path = require("path");
+var express = require('express');
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + "/"));
+//app.use(express.static(__dirname + "/"));
+//app.use(connect.bodyDecoder());
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+app.post('/', function (req, res) {
+  res.render('home');
+});
+
+app.get('/t4', function (req, res) {
+	var retorno = "";
+
+	if(req.param("isGet") != undefined){
+		retorno = req.param("isGet");
+	}
+  res.render('t4', {tipo: "GET", valor: retorno});
+
+});
+app.post('/t4', function (req, res) {
+  var retorno = "";
+  console.log(retorno+" "+ req.params.isPost);
+if(req.param("isPost") != undefined)
+		retorno = req.param("isPost");
+  res.render('t4', {tipo: "POST", valor: retorno});
+});
+
+
+
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3000!');
+});
